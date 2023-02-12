0e4ba0f0320ec2b69b879627698a60b04cb0da36
YAQ256 <christianyacoub@gmail.com>
2023-02-12 18:10:14 +0100

    .

--- write_map.c
+++ write_map.c
@@ -6,7 +6,7 @@
 /*   By: cyacoub- <cyacoub-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 16:20:44 by cyacoub-          #+#    #+#             */
-/*   Updated: 2023/02/11 17:06:26 by cyacoub-         ###   ########.fr       */
+/*   Updated: 2023/02/12 18:05:04 by cyacoub-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,20 @@ void	write_map_w(t_game *game)
 	int k;
 
 	i = 0;
-	j = 0;
-	while (game->map.height)
+	k = 0;
+	while (i < game->map.height)
 	{
-		k = 0;
-		while (game->map.width)
+		j = 0;
+		while (j < game->map.width)
 		{
+			if (game->map.line[k] == '1')
+				mlx_put_image_to_window(game->mlx, game->win, game->map.wall, j * 32, i * 32);
+			else if (game->map.line[k] == 'P')
+				mlx_put_image_to_window(game->mlx, game->win, game->player.left, j * 32, i * 32);
+			/* else if (game->map.line[k] == 'E')
+				mlx_put_image_to_window(game->mlx, game->win, game->map.cell, j * 32, i * 32); */
+			else
+				mlx_put_image_to_window(game->mlx, game->win, game->map.cell, j * 32, i * 32);
 			k++;
 			j++;
 		}
